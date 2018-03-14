cc35e68865c627bbe44af9aa6973dd8ef3cbe4de
Shady Khattab <shady@myparcel.com>
2018-03-14 18:01:56 +0100

    ✨ Added conditional requirement validation

---
+++ app/Validation/PathResolver.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace MyParcelCom\Microservice\Validation;
+
+use stdClass;
+
+class PathResolver
+{
+    /**
+     * Find given path in given data and return the value found. Returns `null`
+     * if the path doesn't exist or the value is `null`.
+     *
+     * @param string   $path
+     * @param stdClass $data
+     * @return mixed
+     */
+    public function resolve(string $path, stdClass $data)
+    {
+        $pathArray = explode('.', $path);
+
+        $property = $data;
+
+        foreach ($pathArray as $attribute)  {
+            $property = $property->$attribute ?? null;
+        };
+
+        return $property;
+    }
+}
