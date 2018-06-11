f823071d4c0dd7ab9526f5861f33ddd038cd15f7
Dennis Koster <dennis@myparcel.com>
2018-06-11 11:58:16 +0200

    :ok_hand: Use assertJsonSchema

--- tests/Endpoints/ValidateCredentialsTest.php
+++ tests/Endpoints/ValidateCredentialsTest.php
@@ -6,6 +6,7 @@ namespace MyParcelCom\Microservice\Tests\Endpoints;
 
 use MyParcelCom\Microservice\Tests\TestCase;
 use MyParcelCom\Microservice\Tests\Traits\CommunicatesWithCarrier;
+use MyParcelCom\Microservice\Tests\Traits\JsonApiAssertionsTrait;
 
 /**
  * @group Endpoints:Shipment
@@ -14,23 +15,39 @@ use MyParcelCom\Microservice\Tests\Traits\CommunicatesWithCarrier;
 class ValidateCredentialsTest extends TestCase
 {
     use CommunicatesWithCarrier;
+    use JsonApiAssertionsTrait;
 
     /** @test */
     public function testItReturnsAnUnsuccessfulResponseWithInvalidCredentials()
     {
         $this->bindCarrierApiGatewayMock();
 
-        $response = $this->json('GET', '/v1/validate-credentials', [], [
-            'foo' => 'bar',
-            'some' => 'credentials'
-        ]);
-        $response->assertStatus(400);
+        $this->assertJsonSchema(
+            '/validate-credentials',
+            '/v1/validate-credentials',
+            [
+                'foo' => 'bar',
+                'some' => 'credentials'
+            ],
+            [],
+            'get',
+            400
+        );
     }
 
     /** @test */
     public function testItReturnsASuccessfulResponseWithValidCredentials()
     {
         $this->bindCarrierApiGatewayMock();
+
+        $this->assertJsonSchema(
+            '/validate-credentials',
+            '/v1/validate-credentials',
+            $this->getRequestHeaders(),
+            [],
+            'get',
+            200
+        );
 
         $response = $this->json('GET', '/v1/validate-credentials', [], $this->getRequestHeaders());
         $response->assertStatus(200);
