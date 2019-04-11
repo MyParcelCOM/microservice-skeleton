333288eeac0c72f9cffae442b3e54b007b43c749
Nick de Vries <nick@myparcel.com>
2019-04-11 18:56:23 +0200

    :white_check_mark::construction: Added ShipmentRequestMock and started writing tests for ShipmentRequests.

--- tests/Endpoints/ShipmentsTest.php
+++ tests/Endpoints/ShipmentsTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace MyParcelCom\Microservice\Tests\Endpoints;
 
 use Mockery;
+use MyParcelCom\Hermes\Http\ShipmentRequest;
+use MyParcelCom\Microservice\Http\Request;
+use MyParcelCom\Microservice\Tests\Mocks\ShipmentRequestMock;
 use MyParcelCom\Microservice\Tests\TestCase;
 use MyParcelCom\Microservice\Tests\Traits\CommunicatesWithCarrier;
 use MyParcelCom\Microservice\Tests\Traits\JsonApiAssertionsTrait;
@@ -22,6 +25,9 @@ class ShipmentsTest extends TestCase
     {
         parent::tearDown();
         Mockery::close();
+
+        $this->app->singleton(ShipmentRequest::class, ShipmentRequestMock::class);
+        $this->bindCarrierApiGatewayMock();
     }
 
     /** @test */
@@ -46,4 +52,23 @@ class ShipmentsTest extends TestCase
             201
         );
     }
+
+    /** @test */
+    public function testItValidatesAShipmentRequestBasedOnValidationRules()
+    {
+        $this->bindCarrierApiGatewayMock();
+
+        $data = \GuzzleHttp\json_decode(
+            file_get_contents(base_path('tests/Stubs/shipment-request.stub')),
+            true
+        );
+
+        $response = $this->json('post', '/shipments',$data, $this->getRequestHeaders());
+
+        // TODO: Add assertions!
+    }
+
+    // TODO: Add test for when validation passes.
+
+    // TODO: Add tests for when validation fails for each case :) (see ShipmentRequestMock for the rules)
 }
---
+++ tests/Mocks/ShipmentRequestMock.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelCom\Microservice\Tests\Mocks;
+
+use MyParcelCom\Hermes\Http\ShipmentRequest;
+
+class ShipmentRequestMock extends ShipmentRequest
+{
+    /**
+     * @return array
+     */
+    public function getRules(): array
+    {
+        return [
+            'data.attributes.recipient_address.email'        => 'required_with:data.attributes.customs|email',
+            'data.attributes.description'                    => 'required|max:15',
+            'data.attributes.recipient_address.phone_number' => 'required_without:data.attributes.recipient_address.email|between:8,15|regex:/^\+?[0-9-]*$/',
+            'data.attributes.service.code'                   => 'required|in:service-a,service-b,service-c',
+            'data.attributes.sender_address.phone_number'    => 'required_if:data.attributes.recipient_address.country_code,CI,data.attributes.recipient_address.region_code,ENG',
+            'data.attributes.items.*.hs_code'                => 'required',
+            'data.attributes.items.*.item_weight'            => 'min:1000',
+        ];
+    }
+}
