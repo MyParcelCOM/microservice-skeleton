2e8f7d94e56b948865eb5faa3d2a132a9c918416
nessy <thomas.eilering@gmail.com>
2020-02-28 15:00:33 +0100

    :ok_hand: Remove json responce string

--- app/Shipments/ShipmentController.php
+++ app/Shipments/ShipmentController.php
@@ -50,8 +50,7 @@ class ShipmentController extends Controller
         $voidedSuccess = false; // TODO: implement void / cancel functionality if necessary
 
         return new JsonResponse(
-            $voidedSuccess ? 'The shipment has been voided.' : 'The shipment could not be voided.',
-            $voidedSuccess ? JsonResponse::HTTP_NO_CONTENT : JsonResponse::HTTP_UNPROCESSABLE_ENTITY
+            '', $voidedSuccess ? JsonResponse::HTTP_NO_CONTENT : JsonResponse::HTTP_LOCKED
         );
     }
 }
