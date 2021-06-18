040dbd37a9e07fb94d495cecc1039962d04b2239
Wesley Stessens <wesley@syn.io>
2021-06-18 11:36:35 +0200

    :sparkles: Support arrays (foo.*.bar) when sanitizing

--- app/Rules/Sanitization/MaxMultipliedSanitization.php
+++ app/Rules/Sanitization/MaxMultipliedSanitization.php
@@ -32,17 +32,32 @@ class MaxMultipliedSanitization implements SanitizationInterface
      */
     public function sanitize(string $key, array $parameters): array
     {
-        // Multiply all values
-        $multiplied = array_reduce($this->fieldKeys, function ($carry, $field) use ($parameters) {
-            return $carry * Arr::get($parameters, $field, 1);
-        }, 1);
-
-        // Correct individual fields if needed
-        if ($multiplied > $this->maxMultiplied) {
-            foreach ($this->fieldKeys as $field) {
-                Arr::set($parameters, $field, strval(floor($this->maxMultiplied ** (1.0 / count($this->fieldKeys)))));
+        $itemKeys = [];
+        foreach ($this->fieldKeys as $fieldKey) {
+            $origValues = data_get($parameters, $fieldKey);
+            if (is_array($origValues)) {
+                $itemKeys += array_keys($origValues);
+            } else {
+                $itemKeys += [0];
             }
         }
+
+        foreach ($itemKeys as $itemKey) {
+            // Multiply all values
+            $multiplied = array_reduce($this->fieldKeys, function ($carry, $field) use ($parameters, $itemKey) {
+                $field = str_replace('*', $itemKey, $field);
+                return $carry * Arr::get($parameters, $field, 1);
+            }, 1);
+
+            // Correct individual fields if needed
+            if ($multiplied > $this->maxMultiplied) {
+                foreach ($this->fieldKeys as $field) {
+                    $field = str_replace('*', $itemKey, $field);
+                    Arr::set($parameters, $field, strval(floor($this->maxMultiplied ** (1.0 / count($this->fieldKeys)))));
+                }
+            }
+        }
+
         return $parameters;
     }
 }
--- app/Rules/Sanitization/MaxSumSanitization.php
+++ app/Rules/Sanitization/MaxSumSanitization.php
@@ -32,17 +32,32 @@ class MaxSumSanitization implements SanitizationInterface
      */
     public function sanitize(string $key, array $parameters): array
     {
-        // Sum all values
-        $sum = array_reduce($this->fieldKeys, function ($carry, $field) use ($parameters) {
-            return $carry + Arr::get($parameters, $field, 0);
-        }, 0);
-
-        // Correct individual fields if needed
-        if ($sum > $this->maxSum) {
-            foreach ($this->fieldKeys as $field) {
-                Arr::set($parameters, $field, strval(floor($this->maxSum / count($this->fieldKeys))));
+        $itemKeys = [];
+        foreach ($this->fieldKeys as $fieldKey) {
+            $origValues = data_get($parameters, $fieldKey);
+            if (is_array($origValues)) {
+                $itemKeys += array_keys($origValues);
+            } else {
+                $itemKeys += [0];
             }
         }
+
+        foreach ($itemKeys as $itemKey) {
+            // Sum all values
+            $sum = array_reduce($this->fieldKeys, function ($carry, $field) use ($parameters, $itemKey) {
+                $field = str_replace('*', $itemKey, $field);
+                return $carry + data_get($parameters, $field, 0);
+            }, 0);
+
+            // Correct individual fields if needed
+            if ($sum > $this->maxSum) {
+                foreach ($this->fieldKeys as $field) {
+                    $field = str_replace('*', $itemKey, $field);
+                    Arr::set($parameters, $field, strval(floor($this->maxSum / count($this->fieldKeys))));
+                }
+            }
+        }
+
         return $parameters;
     }
 }
