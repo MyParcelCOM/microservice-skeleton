5c119ac135efed6773070e049dc93977e7c02ac5
Wesley Stessens <wesley@syn.io>
2021-06-18 11:36:54 +0200

    :white_check_mark: Test sanitization on arrays (foo.*.bar)

--- tests/Unit/Rules/SanitizationTest.php
+++ tests/Unit/Rules/SanitizationTest.php
@@ -27,6 +27,18 @@ class SanitizationTest extends TestCase
         $sanitization = new MaxCharsSanitization(10);
         $sanitized = $sanitization->sanitize('test.input', ['test' => ['input' => '1234567890a']]);
         $this->assertEquals('1234567890', data_get($sanitized, 'test.input'));
+
+        // Test that long input gets cut off correctly for arrays
+        $sanitization = new MaxCharsSanitization(10);
+        $sanitized = $sanitization->sanitize('test.*.input', ['test' => [
+            [
+                'input' => '1234567890a',
+            ], [
+                'input' => '1234567890b',
+            ],
+        ]]);
+        $this->assertEquals('1234567890', data_get($sanitized, 'test.0.input'));
+        $this->assertEquals('1234567890', data_get($sanitized, 'test.1.input'));
     }
 
     /** @test */
@@ -45,6 +57,18 @@ class SanitizationTest extends TestCase
         $this->assertEquals(10, data_get($sanitized, 'test.input'));
         $sanitized = $sanitization->sanitize('test.input', ['test' => ['input' => 101]]);
         $this->assertEquals(100, data_get($sanitized, 'test.input'));
+
+        // Test that input outside the valid range gets corrected for arrays
+        $sanitization = new WithinRangeSanitization(10, 100);
+        $sanitized = $sanitization->sanitize('test.*.input', ['test' => [
+            [
+                'input' => 9,
+            ], [
+                'input' => 101,
+            ],
+        ]]);
+        $this->assertEquals(10, data_get($sanitized, 'test.0.input'));
+        $this->assertEquals(100, data_get($sanitized, 'test.1.input'));
     }
 
     /** @test */
@@ -55,13 +79,13 @@ class SanitizationTest extends TestCase
             'test.input1',
             'test.input2',
         ], '::');
-        $sanitized = $sanitization->sanitize('test.input', ['test' => [
+        $sanitized = $sanitization->sanitize('0', ['test' => [
             'input1' => 'abcd',
             'input2' => 'fghi',
         ]]);
         $this->assertEquals('abcd', data_get($sanitized, 'test.input1'));
         $this->assertEquals('fghi', data_get($sanitized, 'test.input2'));
-        $sanitized = $sanitization->sanitize('test.input', ['test' => [
+        $sanitized = $sanitization->sanitize('0', ['test' => [
             'input1' => 'abc',
             'input2' => 'fgh',
         ]]);
@@ -73,13 +97,32 @@ class SanitizationTest extends TestCase
             'test.input1',
             'test.input2',
         ], '::');
-        $sanitized = $sanitization->sanitize('test.input', ['test' => [
+        $sanitized = $sanitization->sanitize('0', ['test' => [
             'input1' => 'abcdef',
             'input2' => 'ghijkl',
         ]]);
         $this->assertEquals('abcdef', data_get($sanitized, 'test.input1'));
         // Only 2 chars because 6 (input1) + spacer (2) + 2 = 10 (max)
         $this->assertEquals('gh', data_get($sanitized, 'test.input2'));
+
+        // Test that long input gets cut off correctly for arrays
+        $sanitization = new MaxCharsCombinedSanitization(10, [
+            'test.*.input1',
+            'test.*.input2',
+        ], '::');
+        $sanitized = $sanitization->sanitize('0', ['test' => [
+            [
+                'input1' => 'abcdef',
+                'input2' => 'ghijkl',
+            ], [
+                'input1' => 'mnopqr',
+                'input2' => 'stovuw',
+            ],
+        ]]);
+        $this->assertEquals('abcdef', data_get($sanitized, 'test.0.input1'));
+        $this->assertEquals('gh', data_get($sanitized, 'test.0.input2'));
+        $this->assertEquals('mnopqr', data_get($sanitized, 'test.1.input1'));
+        $this->assertEquals('st', data_get($sanitized, 'test.1.input2'));
     }
 
     /** @test */
@@ -114,6 +157,30 @@ class SanitizationTest extends TestCase
         $this->assertEquals(3, data_get($sanitized, 'test.input1'));
         $this->assertEquals(3, data_get($sanitized, 'test.input2'));
         $this->assertEquals(3, data_get($sanitized, 'test.input3'));
+
+        // Test that wrong input gets corrected for arrays
+        $sanitization = new MaxSumSanitization(10, [
+            'test.*.input1',
+            'test.*.input2',
+            'test.*.input3',
+        ]);
+        $sanitized = $sanitization->sanitize('test.input', ['test' => [
+            [
+                'input1' => 3,
+                'input2' => 5,
+                'input3' => 3,
+            ], [
+                'input1' => 9,
+                'input2' => 9,
+                'input3' => 9,
+            ],
+        ]]);
+        $this->assertEquals(3, data_get($sanitized, 'test.0.input1'));
+        $this->assertEquals(3, data_get($sanitized, 'test.0.input2'));
+        $this->assertEquals(3, data_get($sanitized, 'test.0.input3'));
+        $this->assertEquals(3, data_get($sanitized, 'test.1.input1'));
+        $this->assertEquals(3, data_get($sanitized, 'test.1.input2'));
+        $this->assertEquals(3, data_get($sanitized, 'test.1.input3'));
     }
 
     /** @test */
@@ -148,5 +215,29 @@ class SanitizationTest extends TestCase
         $this->assertEquals(2, data_get($sanitized, 'test.input1'));
         $this->assertEquals(2, data_get($sanitized, 'test.input2'));
         $this->assertEquals(2, data_get($sanitized, 'test.input3'));
+
+        // Test that wrong input gets corrected for arrays
+        $sanitization = new MaxMultipliedSanitization(23, [
+            'test.*.input1',
+            'test.*.input2',
+            'test.*.input3',
+        ]);
+        $sanitized = $sanitization->sanitize('test.input', ['test' => [
+            [
+                'input1' => 2,
+                'input2' => 3,
+                'input3' => 4,
+            ], [
+                'input1' => 9,
+                'input2' => 9,
+                'input3' => 9,
+            ],
+        ]]);
+        $this->assertEquals(2, data_get($sanitized, 'test.0.input1'));
+        $this->assertEquals(2, data_get($sanitized, 'test.0.input2'));
+        $this->assertEquals(2, data_get($sanitized, 'test.0.input3'));
+        $this->assertEquals(2, data_get($sanitized, 'test.1.input1'));
+        $this->assertEquals(2, data_get($sanitized, 'test.1.input2'));
+        $this->assertEquals(2, data_get($sanitized, 'test.1.input3'));
     }
 }
