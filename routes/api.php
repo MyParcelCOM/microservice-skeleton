b4efa320ae98319e12e8bbea0e450f2c30323cc9
Wesley Stessens <wesley@syn.io>
2021-06-21 16:23:26 +0200

    :recycle: Refactor routes a little

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,23 @@ use MyParcelCom\Microservice\Statuses\StatusController;
 Route::get('/', function () {
     return new JsonResponse([
         'meta' => [
-            'title'  => 'MyParcel.com Microservice',
+            'title'  => config('app.name'),
             'status' => 'OK',
         ],
     ]);
 });
 
-Route::get('/pickup-dropoff-locations/{countryCode}/{postalCode}', PickUpDropOffLocationController::class . '@getAll');
+Route::get('/pickup-dropoff-locations/{countryCode}/{postalCode}', [PickUpDropOffLocationController::class, 'getAll'])
+    ->name('get-pickup-dropoff-locations');
 
-Route::get('/shipments/{shipmentId}/statuses/{trackingCode}', StatusController::class . '@getStatuses');
-Route::delete('/shipments/{shipmentId}', ShipmentController::class . '@void');
-Route::post('/shipments', [
-    'uses' => ShipmentController::class . '@create',
-    'as'   => 'create-shipment',
-]);
-Route::get('/validate-credentials', CredentialController::class . '@validateCredentials');
+Route::get('/shipments/{shipmentId}/statuses/{trackingCode}', [StatusController::class, 'getStatuses'])
+    ->name('get-statuses');
+
+Route::delete('/shipments/{shipmentId}', [ShipmentController::class, 'void'])
+    ->name('void-shipment');
+
+Route::post('/shipments', [ShipmentController::class, 'create'])
+    ->name('create-shipment');
+
+Route::get('/validate-credentials', [CredentialController::class, 'validateCredentials'])
+    ->name('validate-credentials');
