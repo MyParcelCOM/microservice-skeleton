86615031f8edd1e8b3b60c8980c3ea2dc966008e
Martin Boer <martin@myparcel.com>
2021-06-25 15:59:46 +0200

    :wastebasket: deprecate old tax number functionality

--- app/Shipments/Shipment.php
+++ app/Shipments/Shipment.php
@@ -11,85 +11,61 @@ class Shipment
     public const LABEL_MIME_TYPE_PDF = 'application/pdf';
     public const LABEL_MIME_TYPE_ZPL = 'application/zpl';
 
-    /** @var string */
     protected string $id;
 
-    /** @var Address */
     protected Address $recipientAddress;
 
-    /** @var array */
     protected array $recipientTaxIdentificationNumbers;
 
-    /** @var string|null */
     protected ?string $recipientTaxNumber;
 
-    /** @var Address */
     protected Address $senderAddress;
 
-    /** @var array */
     protected array $senderTaxIdentificationNumbers;
 
-    /** @var string|null */
     protected ?string $senderTaxNumber;
 
-    /** @var Address */
     protected Address $returnAddress;
 
-    /** @var string|null */
     protected ?string $pickupLocationCode;
 
-    /** @var Address|null */
     protected ?Address $pickupLocationAddress;
 
-    /** @var string|null */
     protected ?string $description;
 
-    /** @var int|null */
     protected ?int $totalValueAmount;
 
-    /** @var string|null */
     protected ?string $totalValueCurrency;
 
-    /** @var string|null */
     protected ?string $trackingCode;
 
-    /** @var string|null */
     protected ?string $trackingUrl;
 
-    /** @var string|null */
     protected ?string $barcode;
 
-    /** @var Service */
     protected Service $service;
 
     /** @var Option[] */
     protected array $options = [];
 
-    /** @var PhysicalProperties */
     protected PhysicalProperties $physicalProperties;
 
     /** @var File[] */
     protected array $files = [];
 
-    /** @var Customs|null */
     protected ?Customs $customs;
 
     /** @var ShipmentItem[] */
     protected array $items = [];
 
-    /** @var bool */
     protected bool $trackTraceEnabled = true;
 
-    /** @var string */
     protected string $myparcelcomShipmentId;
 
-    /** @var string */
     protected string $labelMimeType = self::LABEL_MIME_TYPE_PDF;
 
-    /** @var string|null */
     protected ?string $channel;
 
-    /** @var array */
     protected array $taxIdentificationNumbers = [];
 
     /**
@@ -500,6 +476,7 @@ class Shipment
 
     /**
      * @return string|null
+     * @deprecated
      */
     public function getRecipientTaxNumber(): ?string
     {
@@ -509,6 +486,7 @@ class Shipment
     /**
      * @param string $recipientTaxNumber
      * @return $this
+     * @deprecated
      */
     public function setRecipientTaxNumber(string $recipientTaxNumber): self
     {
@@ -538,6 +516,7 @@ class Shipment
 
     /**
      * @return string|null
+     * @deprecated
      */
     public function getSenderTaxNumber(): ?string
     {
@@ -547,6 +526,7 @@ class Shipment
     /**
      * @param string $senderTaxNumber
      * @return $this
+     * @deprecated
      */
     public function setSenderTaxNumber(string $senderTaxNumber): self
     {
@@ -648,6 +628,7 @@ class Shipment
     /**
      * @param array $taxIdentificationNumbers
      * @return $this
+     * @deprecated
      */
     public function setTaxIdentificationNumbers(array $taxIdentificationNumbers): self
     {
@@ -658,6 +639,7 @@ class Shipment
 
     /**
      * @return array
+     * @deprecated
      */
     public function getTaxIdentificationNumbers(): array
     {
@@ -668,6 +650,7 @@ class Shipment
      * @param string $type
      * @param string $countryCode
      * @return string|null
+     * @deprecated
      */
     public function getTaxIdentificationNumber(string $type, string $countryCode): ?string
     {
