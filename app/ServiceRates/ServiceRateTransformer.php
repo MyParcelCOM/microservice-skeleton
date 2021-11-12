8aaf30dbb5a1874459fcab4819513bc06ab9bc1c
Nick Zwaans <nick.zwaans@myparcel.com>
2021-11-12 11:04:40 +0100

    :necktie: fixed the getId function on the transformer to return null. Added transformer tests, updated getAttributes of transformer to correctly transform the price objects

--- app/ServiceRates/ServiceRateTransformer.php
+++ app/ServiceRates/ServiceRateTransformer.php
@@ -7,30 +7,52 @@ namespace MyParcelCom\Microservice\ServiceRates;
 use MyParcelCom\JsonApi\Exceptions\ModelTypeException;
 use MyParcelCom\JsonApi\Transformers\AbstractTransformer;
 
-class ServiceRateTransformer extends AbstractTransformer {
+class ServiceRateTransformer extends AbstractTransformer
+{
 
     /** @var string */
     protected $type = 'service_rates';
 
     /**
      * @param mixed $serviceRate
-     * @return string
+     * @return string|null
      */
-    public function getId($serviceRate): string
+    public function getId($serviceRate): ?string
     {
         $this->validateModel($serviceRate);
 
-        return $serviceRate->getId();
+        return null;
     }
 
     /**
-     * @param mixed $serviceRate
+     * @param ServiceRate $serviceRate
      * @return array
      */
     public function getAttributes($serviceRate): array
     {
         $this->validateModel($serviceRate);
-        return parent::getAttributes($serviceRate);
+
+        return array_filter([
+            'code'           => $serviceRate->getCode(),
+            'weight_min'     => $serviceRate->getWeightMin(),
+            'weight_max'     => $serviceRate->getWeightMax(),
+            'length_max'     => $serviceRate->getLengthMax(),
+            'width_max'      => $serviceRate->getWidthMax(),
+            'height_max'     => $serviceRate->getHeightMax(),
+            'volume_max'     => $serviceRate->getVolumeMax(),
+            'price'          => array_filter([
+                'amount'   => $serviceRate->getPrice()->getAmount(),
+                'currency' => $serviceRate->getPrice()->getCurrency(),
+            ]),
+            'purchase_price' => array_filter([
+                'amount'   => $serviceRate->getPurchasePrice()->getAmount(),
+                'currency' => $serviceRate->getPurchasePrice()->getCurrency(),
+            ]),
+            'fuel_surcharge' => $serviceRate->getFuelSurcharge() === null ? null : array_filter([
+                'amount'   => $serviceRate->getFuelSurcharge()->getAmount(),
+                'currency' => $serviceRate->getFuelSurcharge()->getCurrency(),
+            ]),
+        ]);
     }
 
     /**
