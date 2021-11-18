388fd3f5f7952f6792b770e5e2e8780dad1ca379
Nick Zwaans <nick.zwaans@myparcel.com>
2021-11-18 16:35:08 +0100

    :necktie: updated tests for manifest

--- tests/Unit/Manifests/ManifestTest.php
+++ tests/Unit/Manifests/ManifestTest.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace MyParcelCom\Microservice\Tests\Unit\Manifests;
 
+use MyParcelCom\Microservice\Manifests\Manifest;
 use MyParcelCom\Microservice\Tests\TestCase;
 
 class ManifestTest extends TestCase
 {
+    private Manifest $manifest;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->manifest = new Manifest('my-test-manifest');
+    }
+
+    public function testItGetsAName(): void
+    {
+        $this->assertEquals('my-test-manifest', $this->manifest->getName());
     }
 }
