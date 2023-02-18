d3fde29f8971d03a7f93031b23d702104fd104db
Wesley Stessens <wesley@syn.io>
2023-02-18 14:02:59 +0100

    ✨ Default code to get collection time slots based on public holidays per country

--- app/CollectionTimeSlots/CollectionTimeSlotRepository.php
+++ app/CollectionTimeSlots/CollectionTimeSlotRepository.php
@@ -5,17 +5,37 @@ declare(strict_types=1);
 namespace MyParcelCom\Microservice\CollectionTimeSlots;
 
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+use MyParcelCom\JsonApi\Errors\InvalidInputError;
+use MyParcelCom\JsonApi\Exceptions\InvalidInputException;
+use MyParcelCom\JsonApi\Resources\CollectionResources;
 use MyParcelCom\JsonApi\Resources\Interfaces\ResourcesInterface;
 use MyParcelCom\JsonApi\Resources\PromiseResources;
 use MyParcelCom\Microservice\Carrier\CarrierApiGatewayInterface;
+use RuntimeException;
+use Yasumi\Exception\UnknownLocaleException;
+use Yasumi\Exception\ProviderNotFoundException;
+use ReflectionException;
+use Yasumi\Yasumi;
+
+use function collect;
 
 class CollectionTimeSlotRepository
 {
-    protected CarrierApiGatewayInterface $carrierApiGateway;
+    /**
+     * In-memory cache for holiday checker objects
+     *
+     * @var array
+     */
+    private static array $holidayCheckers = [];
 
-    public function __construct(CarrierApiGatewayInterface $carrierApiGateway)
+    /**
+     * @param CarrierApiGatewayInterface $carrierApiGateway
+     */
+    public function __construct(private CarrierApiGatewayInterface $carrierApiGateway)
     {
-        $this->carrierApiGateway = $carrierApiGateway;
     }
 
     public function getCollectionTimeSlots(
@@ -25,21 +45,78 @@ class CollectionTimeSlotRepository
         Carbon $dateTo,
         ?string $serviceCode = null,
     ): ResourcesInterface {
-        $queryParams = array_filter([
-            'country_code' => $countryCode,
-            'postal_code'  => $postalCode,
-            'date_from'    => $dateFrom->toIso8601String(),
-            'date_to'      => $dateTo->toIso8601String(),
-            'service_code' => $serviceCode,
-        ]);
+        if ($dateFrom->isAfter($dateTo)) {
+            throw new InvalidInputException([
+                new InvalidInputError('', 'Date from cannot be after date to')
+            ]);
+        }
+
+        $period = CarbonPeriod::create($dateFrom, $dateTo);
+        $timeSlots = collect();
+
+        foreach ($period as $date) {
+            if ($date->isWeekend()) {
+                continue;
+            }
+
+            if ($this->isHoliday($countryCode, $date)) {
+                continue;
+            }
+
+            $from = $date->clone()->setTime(9, 0);
+            $to = $date->clone()->setTime(18, 30);
+
+            $timeSlots->add(new CollectionTimeSlot(
+                $from->toIso8601String() . '_' . $to->toIso8601String(),
+                $from,
+                $to
+            ));
+        }
+
+        return new CollectionResources($timeSlots);
+
+        // todo: If carrier API supports time availability checks, then use the CarrierApiGateway instead to get the time slots.
+
+        // $queryParams = array_filter([
+        //     'country_code' => $countryCode,
+        //     'postal_code'  => $postalCode,
+        //     'date_from'    => $dateFrom->toIso8601String(),
+        //     'date_to'      => $dateTo->toIso8601String(),
+        //     'service_code' => $serviceCode,
+        // ]);
 
         // todo: Implement a request to the carrier with the CarrierApiGateway using the queryString.
         // $response = $this->carrierApiGateway->get('url', $queryParams);
 
         // todo: Map the result into CollectionTimeSlot resources.
+        // return new PromiseResources(
+        // // todo: Return the CollectionTimeSlots.
+        // );
+    }
 
-        return new PromiseResources(
-        // todo: Return the CollectionTimeSlots.
-        );
+    /**
+     * @param string $countryCode
+     * @param Carbon $date
+     * @return bool
+     * @throws RuntimeException
+     * @throws InvalidArgumentException
+     * @throws UnknownLocaleException
+     * @throws ProviderNotFoundException
+     * @throws ReflectionException
+     */
+    private function isHoliday(string $countryCode, Carbon $date): bool
+    {
+        $countryCode = strtoupper($countryCode);
+        $year = $date->year;
+
+        if (!Arr::has(self::$holidayCheckers, $countryCode)) {
+            self::$holidayCheckers[$countryCode] = [];
+        }
+
+        if (!Arr::has(self::$holidayCheckers[$countryCode], $year)) {
+            self::$holidayCheckers[$countryCode][$year] = Yasumi::createByISO3166_2($countryCode, $year);
+        }
+
+        return !self::$holidayCheckers[$countryCode][$year]->isWorkingDay($date);
     }
 }
