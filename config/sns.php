5bee664d4b52a8aa91036dadd72103a957fdb419
Yoan-Alexander Grigorov <yoan@myparcel.com>
2023-06-30 10:43:10 +0200

    :ok_hand: Specify more precise config entry for sns statuses topic arn

--- config/sns.php
+++ config/sns.php
@@ -5,5 +5,7 @@ declare(strict_types=1);
 return [
     'region'    => env('AWS_REGION', 'eu-central-1'),
     'version'   => '2010-03-31',
-    'topic_arn' => env('SNS_TOPIC_ARN'),
+    'statuses' => [
+        'topic_arn' => env('SNS_STATUSES_TOPIC_ARN'),
+    ],
 ];
